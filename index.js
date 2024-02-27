a344b666039a25715e92f8c6088585791866e450
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-02-27 21:10:58 +0100

    termux notification + eslint and prettier update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Client, Collection, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 const Sequelize = require('sequelize');
 // const { DataTypes } = require('sequelize');
+const { exec } = require('child_process');
+const util = require('util');
+const execPromise = util.promisify(exec);
 
 // Create a new client instance
 const client = new Client({
@@ -22,17 +25,21 @@ client.sequelize = new Sequelize('database', 'user', 'password', {
 	storage: './database.sqlite',
 });
 
-client.Users = client.sequelize.define('users', {
-	user: {
-		type: Sequelize.STRING,
-		unique: true,
+client.Users = client.sequelize.define(
+	'users',
+	{
+		user: {
+			type: Sequelize.STRING,
+			unique: true,
+		},
+		lastfm: Sequelize.STRING,
+		locked: Sequelize.BOOLEAN,
+		listenbrainz: Sequelize.STRING,
 	},
-	lastfm: Sequelize.STRING,
-	locked: Sequelize.BOOLEAN,
-	listenbrainz: Sequelize.STRING,
-}, {
-	timestamps: false,
-});
+	{
+		timestamps: false,
+	}
+);
 
 // Commands handler
 client.commands = new Collection();
@@ -41,7 +48,9 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -49,14 +58,18 @@ for (const folder of commandFolders) {
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
 		}
 	}
 }
 
 // Events handler
 const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventFiles = fs
+	.readdirSync(eventsPath)
+	.filter((file) => file.endsWith('.js'));
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
@@ -70,3 +83,32 @@ for (const file of eventFiles) {
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
+
+// Scheduled tasks:
+async function termuxBatteryNotif() {
+	let battery = '';
+	try {
+		const { error, stdout, stderr } = await execPromise(
+			'termux-battery-status'
+		);
+		if (error) {
+			console.log(error);
+		}
+		if (stderr) {
+			console.log(stderr);
+		}
+		battery = JSON.parse(stdout);
+
+		if (Number(battery.percentage) < 20 && battery.plugged == 'UNPLUGGED') {
+			client.users.send(
+				process.env.OWNER_ID,
+				`Warning, low battery: ${battery.percentage}%.`
+			);
+		}
+	} catch (error) {
+		console.log(`error: ${error}`);
+	}
+}
+if (process.env.TERMUX) {
+	setInterval(termuxBatteryNotif, 1000 * 60 * 60);
+}
