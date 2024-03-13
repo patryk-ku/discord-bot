57fcc0d2e1680d2ee47941a72f4c86ac21629442
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-03-14 00:24:20 +0100

    new command: "/debug settings"

--- commands/debug/debug.js
+++ commands/debug/debug.js
@@ -9,24 +9,30 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('debug')
 		.setDescription('Get debug info.')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('user')
-				.setDescription('Debug info about a user.'),
+		.addSubcommand(
+			(subcommand) => subcommand.setName('user').setDescription('Debug info about a user.')
 			// .addUserOption(option => option.setName('target').setDescription('The user'))
 		)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('server')
-				.setDescription('Debug info about the server.'))
-		.addSubcommand(subcommand =>
+
+		.addSubcommand((subcommand) =>
+			subcommand.setName('server').setDescription('Debug info about the server.')
+		)
+
+		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('termux')
-				.setDescription('Debug info about the Termux instance (if in use).'))
-		.addSubcommand(subcommand =>
+				.setDescription('Debug info about the Termux instance (if in use).')
+		)
+
+		.addSubcommand((subcommand) =>
+			subcommand.setName('voice').setDescription('Debug info about the voice internals.')
+		)
+
+		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('voice')
-				.setDescription('Debug info about the voice internals.'))
+				.setName('settings')
+				.setDescription('Debug info about bot enabled APIs and settings.')
+		)
 		.setDefaultMemberPermissions(0)
 		.setDMPermission(false),
 	async execute(interaction) {
@@ -34,15 +40,18 @@ module.exports = {
 			case 'user': {
 				// interaction.user is the object representing the User who ran the command
 				// interaction.member is the GuildMember object, which represents the user in the specific guild
-				await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+				await interaction.reply(
+					`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+				);
 				console.log(interaction.user);
 				console.log(interaction);
-				// await interaction.channel.send('dummy message');
 				return;
 			}
 
 			case 'server': {
-				await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+				await interaction.reply(
+					`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+				);
 				console.log(interaction.guild.members);
 
 				// const users = await interaction.guild.members.fetch();
@@ -73,7 +82,10 @@ module.exports = {
 					}
 					console.log(stdout);
 					battery = JSON.parse(stdout);
-					embed.addFields({ name: 'Battery:', value: `**${battery.percentage}%**, ${Number(battery.temperature).toFixed(1)}°C (${battery.plugged}, ${battery.status})` });
+					embed.addFields({
+						name: 'Battery:',
+						value: `**${battery.percentage}%**, ${Number(battery.temperature).toFixed(1)}°C (${battery.plugged}, ${battery.status})`,
+					});
 					isFail = false;
 				} catch (error) {
 					console.log(`error: ${error}`);
@@ -95,7 +107,9 @@ module.exports = {
 				}
 
 				try {
-					const { error, stdout, stderr } = await execPromise('free -m --si | awk \'FNR == 2 {print $3" MB / "$2" MB"}\'');
+					const { error, stdout, stderr } = await execPromise(
+						'free -m --si | awk \'FNR == 2 {print $3" MB / "$2" MB"}\''
+					);
 					if (error) {
 						console.log(error);
 					}
@@ -128,8 +142,64 @@ module.exports = {
 				return interaction.editReply({ content: '', embeds: [embed] });
 			}
 
+			case 'settings': {
+				await interaction.deferReply();
+				const settings = [];
+
+				const add = (name, value) => {
+					settings.push({ name: name, value: value });
+				};
+				const separator = (title) => {
+					settings.push({ separator: true, header: title });
+				};
+
+				// APIs
+				separator('Enabled APIs');
+				add('Last.fm', process.env.LASTFM_API_KEY ? ':white_check_mark:' : ':x:');
+				add('Listenbrainz', process.env.LISTENBRAINZ_TOKEN ? ':white_check_mark:' : ':x:');
+				add('Gemini', process.env.GEMINI_API_KEY ? ':white_check_mark:' : ':x:');
+
+				// Other settings
+				separator('Other settings');
+				add('Voice commands', process.env.VOICE_COMMANDS ? ':white_check_mark:' : ':x:');
+				add(
+					'Global slash commands',
+					process.env.DISCORD_GUILD_ID ? ':x:' : ':white_check_mark:'
+				);
+				add(
+					'Auto restart on connection loss',
+					process.env.INTERNET_LOSS_RESTART ? ':white_check_mark:' : ':x:'
+				);
+				add('Termux mode', process.env.TERMUX ? ':white_check_mark:' : ':x:');
+				add(
+					'Termux low battery notification',
+					process.env.TERMUX_LOW_BATTERY_NOTIF ? ':white_check_mark:' : ':x:'
+				);
+
+				// Bot owner
+				add('Bot instance owner', `<@${process.env.OWNER_ID}>`);
+
+				// Parse into markdown string
+				let embedString = '';
+				for (const option of settings) {
+					if (option.separator) {
+						embedString += `\n### ${option.header}`;
+					} else {
+						embedString += `\n- **${option.name}**:  ${option.value}`;
+					}
+				}
+
+				const embed = new EmbedBuilder()
+					.setDescription(embedString);
+
+				return await interaction.editReply({ content: '', embeds: [embed] });
+			}
+
 			default: {
-				return interaction.reply({ content: 'Error: Missing subcommand.', ephemeral: true });
+				return interaction.reply({
+					content: 'Error: Missing subcommand.',
+					ephemeral: true,
+				});
 			}
 		}
 	},
