02bd425236dcc77634f1eab635c0735946051fad
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-06-27 21:39:15 +0200

    feat: new commands for bot update and branch change

--- commands/config/config.js
+++ commands/config/config.js
@@ -1,52 +1,74 @@
-const { SlashCommandBuilder, ActivityType } = require('discord.js');
+const { SlashCommandBuilder, ActivityType, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('config')
 		.setDescription('Bot config commands')
-		.addSubcommand(subcommand =>
+		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('status')
 				.setDescription('Set bot status.')
-				.addStringOption(option =>
-					option.setName('status')
+				.addStringOption((option) =>
+					option
+						.setName('status')
 						.setDescription('Status option.')
 						.addChoices(
 							{ name: 'online', value: 'online' },
 							{ name: 'idle', value: 'idle' },
 							{ name: 'dnd', value: 'dnd' },
-							{ name: 'invisible', value: 'invisible' },
+							{ name: 'invisible', value: 'invisible' }
 						)
-						.setRequired(true)),
+						.setRequired(true)
+				)
 		)
-		.addSubcommand(subcommand =>
+		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('activity')
 				.setDescription('Set bot activity.')
-				.addStringOption(option =>
-					option.setName('string')
-						.setDescription('Activity string, to clear omit this option.'))
-				.addStringOption(option =>
-					option.setName('type')
+				.addStringOption((option) =>
+					option
+						.setName('string')
+						.setDescription('Activity string, to clear omit this option.')
+				)
+				.addStringOption((option) =>
+					option
+						.setName('type')
 						.setDescription('Activity type.')
 						.addChoices(
 							{ name: 'Playing', value: 'Playing' },
 							{ name: 'Watching', value: 'Watching' },
 							{ name: 'Listening', value: 'Listening' },
 							{ name: 'Competing', value: 'Competing' },
-							{ name: 'Custom', value: 'Custom' },
-						)),
+							{ name: 'Custom', value: 'Custom' }
+						)
+				)
 		)
-		.addSubcommand(subcommand =>
+		.addSubcommand((subcommand) => subcommand.setName('restart').setDescription('Restart bot.'))
+		.addSubcommand((subcommand) =>
+			subcommand.setName('update').setDescription('Git pull from current branch.')
+		)
+		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('restart')
-				.setDescription('Restart bot.'))
+				.setName('branch')
+				.setDescription('Change git branch. ')
+				.addStringOption((option) =>
+					option
+						.setName('name')
+						.setDescription('Branch name')
+						.addChoices({ name: 'main', value: 'main' }, { name: 'dev', value: 'dev' })
+						.setRequired(true)
+				)
+		)
 		.setDefaultMemberPermissions(0)
 		.setDMPermission(true),
 	async execute(interaction) {
 		if (interaction.user.id != process.env.OWNER_ID) {
-			return interaction.reply('These commands can only be used by the owner of an instance of this bot');
+			return interaction.reply(
+				'These commands can only be used by the owner of an instance of this bot'
+			);
 		}
 
 		switch (interaction.options.getSubcommand()) {
@@ -69,22 +91,30 @@ module.exports = {
 				}
 
 				if (!str) {
-					return interaction.editReply('Activity string cannot be empty to set activity type.');
+					return interaction.editReply(
+						'Activity string cannot be empty to set activity type.'
+					);
 				}
 
 				if (type == 'Watching') {
 					await interaction.client.user.setActivity(str, { type: ActivityType.Watching });
 				} else if (type == 'Listening') {
-					await interaction.client.user.setActivity(str, { type: ActivityType.Listening });
+					await interaction.client.user.setActivity(str, {
+						type: ActivityType.Listening,
+					});
 				} else if (type == 'Competing') {
-					await interaction.client.user.setActivity(str, { type: ActivityType.Competing });
+					await interaction.client.user.setActivity(str, {
+						type: ActivityType.Competing,
+					});
 				} else if (type == 'Playing') {
 					await interaction.client.user.setActivity(str, { type: ActivityType.Playing });
 				} else if (type == 'Custom') {
 					await interaction.client.user.setActivity(str, { type: ActivityType.Custom });
 				}
 
-				return interaction.editReply(`Bot activity type set to: **${type}**. Bot status set to: **${str}**.`);
+				return interaction.editReply(
+					`Bot activity type set to: **${type}**. Bot status set to: **${str}**.`
+				);
 			}
 
 			case 'restart': {
@@ -93,8 +123,102 @@ module.exports = {
 				return;
 			}
 
+			case 'update': {
+				await interaction.deferReply();
+
+				const embed = new EmbedBuilder()
+					.setDescription('Executing `git pull`, please wait...');
+
+				await interaction.editReply({ embeds: [embed] });
+
+				let stdout, stderr;
+
+				// First pull updates from current branch
+				try {
+					({ stdout, stderr } = await exec('git pull'));
+				} catch (error) {
+					embed.setDescription(`### Error:\n \`\`\`${error}\`\`\``);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				if (stderr.length > 0) {
+					embed.setDescription(`### Error:\n \`\`\`${stdout}\`\`\``);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				const output = `### \`git pull\` output:\n \`\`\`${stdout}\`\`\``;
+
+				// Return if no updates
+				if (stdout.includes('Already up to date.')) {
+					embed.setDescription(output);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				embed.setDescription(`${output}\nRefreshing commands, please wait...`);
+				await interaction.editReply({ embeds: [embed] });
+
+				// TODO: fix also for 'local' commands
+				// Then refresh commands (works only with global commands)
+				try {
+					({ stdout, stderr } = await exec('npm run deploy-global-commands'));
+				} catch (error) {
+					embed.setDescription(
+						`${output}\n### Refresh commands error:\n \`\`\`${error}\`\`\``
+					);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				if (stderr.length > 0) {
+					embed.setDescription(
+						`${output}\n### Refresh commands error:\n \`\`\`${stderr}\`\`\``
+					);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				embed.setDescription(
+					`${output}\n### \`npm run deploy-global-commands\` output:\n \`\`\`${stdout}\`\`\` \n### Update completed.\nAfter a successful update, manually run the </config restart:1186353226811969728> command.`
+				);
+				await interaction.editReply({ embeds: [embed] });
+
+				return;
+			}
+
+			case 'branch': {
+				await interaction.deferReply();
+				const name = interaction.options.getString('name');
+
+				const embed = new EmbedBuilder()
+					.setDescription(`Executing \`git checkout ${name}\`, please wait...`);
+
+				await interaction.editReply({ embeds: [embed] });
+
+				let stdout, stderr;
+
+				try {
+					({ stdout, stderr } = await exec(`git checkout ${name}`));
+				} catch (error) {
+					embed.setDescription(`### Error:\n \`\`\`${error}\`\`\``);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				if (stderr.length > 0) {
+					embed.setDescription(`### Error:\n \`\`\`${stdout}\`\`\``);
+					return await interaction.editReply({ embeds: [embed] });
+				}
+
+				embed.setDescription(
+					`### Command output:\n \`\`\`${stdout}\`\`\` \nAfter a successful branch change, manually run the </config restart:1186353226811969728> command.`
+				);
+				await interaction.editReply({ embeds: [embed] });
+
+				return;
+			}
+
 			default: {
-				return interaction.reply({ content: 'Error: Missing subcommand.', ephemeral: true });
+				return interaction.reply({
+					content: 'Error: Missing subcommand.',
+					ephemeral: true,
+				});
 			}
 		}
 	},
