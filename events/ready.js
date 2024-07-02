80d37a6b5ecf300213dfe42b4d84a82a7edf6d79
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-07-02 18:57:21 +0200

    fix: set correct NODE_ENV for production and development

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,16 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.users.send(process.env.OWNER_ID, `✅ Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}.`);
+		console.log(`Running in ${process.env.NODE_ENV} mode.`);
+
+		// Notify bot owner about bot instance start
+		if (process.env.NODE_ENV === 'production') {
+			client.users.send(
+				process.env.OWNER_ID,
+				`✅ Ready! Logged in as \`${client.user.tag}\``
+			);
+		}
 
 		try {
 			await client.sequelize.authenticate();
@@ -15,11 +23,15 @@ module.exports = {
 			// await client.geminiChat.sync();
 			await client.sequelize.sync();
 		} catch (error) {
-			console.error('Unable to connect to the database:', error);
-			client.users.send(
-				process.env.OWNER_ID,
-				`❌ Unable to connect to the database: ${error}`
-			);
+			console.error(`Unable to connect to the database: ${error}`);
+
+			// Notify bot owner about database error
+			if (process.env.NODE_ENV === 'production') {
+				client.users.send(
+					process.env.OWNER_ID,
+					`❌ Unable to connect to the database: ${error}`
+				);
+			}
 		}
 	},
 };
